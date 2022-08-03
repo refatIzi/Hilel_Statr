8d5294cd2315deae6a4f50880507ea4cac25a6ca
refat <refaazer@gmail.com>
2022-08-03 22:42:23 +0300

    alishev

--- src/main/java/homwork/HomWork3.java
+++ src/main/java/homwork/HomWork3.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 
 public class HomWork3 {
     public static void main(String[] args) throws IOException {
-        //Scanner scanner = new Scanner(System.in);
-        MyScanner_ scanner = new MyScanner_(System.in);
+        Scanner scanner = new Scanner(System.in);
+        //MyScanner_ scanner = new MyScanner_(System.in);
         System.out.println("Введите целое число");
         int a = scanner.nextInt();
         System.out.println("Введите дробное число");
         double b = scanner.nextDouble();
         System.out.println("Введите Ваше имя");
-        //scanner.nextLine(); уже не нужно
+        scanner.nextLine();
         String name = scanner.nextLine();
         System.out.println(name);
         System.out.printf("Привет %1$s! %n%1$s, Ти ввел Целое число %2$s! и Дробное %3$ 01.5f" +
