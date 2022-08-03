375187996b35279c4dbf7accac6d12234ea863ee
refat <refaazer@gmail.com>
2022-08-03 23:33:34 +0300

    alishev

---
+++ src/main/java/alishev/Class_and_Object.java
@@ -0,0 +1,23 @@
+package alishev;
+
+public class Class_and_Object {
+    public static void main(String[] args) {
+        Info infoA = new Info();
+        infoA.firstName = "Вадим";
+        infoA.lastName = "Незабиваемий";
+        infoA.age = 30;
+        Info infoB = new Info();
+        infoB.firstName = "Лиля";
+        infoB.lastName = "Заболотная";
+        infoB.age = 30;
+        System.out.println("My Name " + infoA.firstName + " " + infoA.lastName + " age " + infoA.age);
+        System.out.println("My Name " + infoB.firstName + " " + infoB.lastName + " age " + infoB.age);
+
+    }
+}
+
+class Info {
+    String firstName;
+    String lastName;
+    int age;
+}
