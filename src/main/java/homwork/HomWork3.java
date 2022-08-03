c7d72c930580f57c0ce99facb8746f2120d07211
refat <refaazer@gmail.com>
2022-08-03 22:43:21 +0300

    alishev

--- src/main/java/homwork/HomWork3.java
+++ src/main/java/homwork/HomWork3.java
@@ -16,8 +16,7 @@ public class HomWork3 {
         System.out.println("Введите Ваше имя");
         scanner.nextLine();
         String name = scanner.nextLine();
-        System.out.println(name);
-        System.out.printf("Привет %1$s! %n%1$s, Ти ввел Целое число %2$s! и Дробное %3$ 01.5f" +
-                " %n Сумма єтих чисел равна %4$+01.5f %n Деление єтих чисел равна %5$ 01.5f", name, a, b, a + b,a/b);
+        System.out.printf("Привет %1$s! %n%1$s, Ти ввел Целое число %2$s! и Дробное %3$ 01.5f"
+                + " %n Сумма єтих чисел равна %4$+01.5f %n Деление єтих чисел равна %5$ 01.5f", name, a, b, a + b, a / b);
     }
 }
