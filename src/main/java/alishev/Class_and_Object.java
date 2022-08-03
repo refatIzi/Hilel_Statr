0a96f5a5c13e4d28b0f7c1a48303d20dce12c299
refat <refaazer@gmail.com>
2022-08-03 23:36:14 +0300

    alishev

--- src/main/java/alishev/Class_and_Object.java
+++ src/main/java/alishev/Class_and_Object.java
@@ -4,11 +4,11 @@ public class Class_and_Object {
     public static void main(String[] args) {
         Info infoA = new Info();
         infoA.firstName = "Вадим";
-        infoA.lastName = "Незабиваемий";
+        infoA.lastName = "Незабутній";
         infoA.age = 30;
         Info infoB = new Info();
         infoB.firstName = "Лиля";
-        infoB.lastName = "Заболотная";
+        infoB.lastName = "Заболотна";
         infoB.age = 30;
         System.out.println("My Name " + infoA.firstName + " " + infoA.lastName + " age " + infoA.age);
         System.out.println("My Name " + infoB.firstName + " " + infoB.lastName + " age " + infoB.age);
