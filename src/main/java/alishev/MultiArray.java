bd52b614ffd31b94f58ea274aea8ac3518025687
refat <refaazer@gmail.com>
2022-08-03 23:15:34 +0300

    alishev

---
+++ src/main/java/alishev/MultiArray.java
@@ -0,0 +1,22 @@
+package alishev;
+
+import java.util.Random;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        int[][] array = new int[5][10];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++)
+                array[i][j] = new Random().nextInt(100);
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i+1+ " {");
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print("[" + array[i][j]+"]");
+            }
+            System.out.println("}");
+        }
+    }
+}
