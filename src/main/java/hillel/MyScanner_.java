bc5e44d35e605b98b2061ed8f1052e9715ae5510
refat <refaazer@gmail.com>
2022-08-03 19:12:32 +0300

    alishev

--- src/main/java/hillel/MyScanner_.java
+++ src/main/java/hillel/MyScanner_.java
@@ -36,7 +36,7 @@ public class MyScanner_ {
         byte[] buff = new byte[1024];
         int read = in.read(buff);
         // System.out.println("Size buffer "+read);
-        /**Добавляем в buff данние уменшая ращмер на -1 та как последний єлемен Команда для опускания каретки печатающего устройства на одну строку вниз
+        /**Добавляем в buff данние уменшая ращмер на -1 та как последний єлемен Команда для опускания каретки на одну строку вниз
          * она нам не нужна*/
         return new String(Arrays.copyOf(buff, read - 1));
     }
