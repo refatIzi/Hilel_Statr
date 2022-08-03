e4299e0b9ea7cd0f0d958409798d91736f20f62a
refat <refaazer@gmail.com>
2022-08-03 23:15:53 +0300

    alishev

--- src/main/java/alishev/MultiArray.java
+++ src/main/java/alishev/MultiArray.java
@@ -12,9 +12,9 @@ public class MultiArray {
 
 
         for (int i = 0; i < array.length; i++) {
-            System.out.print(i+1+ " {");
+            System.out.print(i + 1 + " {");
             for (int j = 0; j < array[0].length; j++) {
-                System.out.print("[" + array[i][j]+"]");
+                System.out.print("[" + array[i][j] + "]");
             }
             System.out.println("}");
         }
