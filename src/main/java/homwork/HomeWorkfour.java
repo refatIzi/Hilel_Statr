401b44db40b2904a27b2ad5539c887ca06f4e306
refat <refaazer@gmail.com>
2022-08-07 19:21:51 +0300

    alishev

--- src/main/java/homwork/HomeWorkfour.java
+++ src/main/java/homwork/HomeWorkfour.java
@@ -12,7 +12,7 @@ public class HomeWorkfour {
         String command;
         int number = 0;
         String name = "";
-        System.out.println("Вы вошли в картотеку абонентов для ввода данных введите 'a':");
+        help();
         while (true) {
             try {
                 command = scanner.nextLine();
@@ -22,22 +22,22 @@ public class HomeWorkfour {
                     scanner.nextLine();
                     System.out.println("Введите имя");
                     name = scanner.nextLine();
-                    System.out.println("Для того чтобы начать заново вводить данные введиет 'a'\n"
-                            + "Для сохранения  изменения и просомтра введиет 'w'\n"
-                            + "Для просомтра данных введиет 'r'\n"
-                            + "Для сохранения  изменения введите 's'\n"
-                            + "Для выхода из системы без изменений введите 'q'");
+                    System.out.println("Введите команду 's' для сохранения или 'w' для сохранения и просмотра:");
+
                 } else if (command.equals("w")) {
                     map.put(number, name);
                     showPerson();
                     System.out.println("Введите команду:");
                     // System.out.println(map.entrySet());
+                } else if (command.equals("r")) {
+                    showPerson();
+                    System.out.println("Введите команду:");
                 } else if (command.equals("s")) {
                     map.put(number, name);
                     System.out.println("Данные сохранены");
                     System.out.println("Введите команду:");
-                } else if (command.equals("t")) {
-                    showPerson();
+                } else if (command.equals("h")) {
+                    help();
                     System.out.println("Введите команду:");
                 } else if (command.equals("q")) {
                     System.out.println("Досвидание!!!!!!!!!");
@@ -50,6 +50,15 @@ public class HomeWorkfour {
                 scanner.next();
             }
         }
+    }
+
+    public static void help() {
+        System.out.println("Для того чтобы начать вводить данные введиет 'a'\n"
+                + "Для сохранения  изменения и просомтра введиет 'w'\n"
+                + "Для просомтра данных введиет 'r'\n"
+                + "Для сохранения  изменения введите 's'\n"
+                + "Для простмора команд введите 'h'\n"
+                + "Для выхода из системы без изменений введите 'q'");
     }
 
     public static void showPerson() {
