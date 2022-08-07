d9c221473359727504f027f88454ad61e83da5d3
refat <refaazer@gmail.com>
2022-08-07 19:37:13 +0300

    alishev

--- src/main/java/homwork/HomeWorkfour_.java
+++ src/main/java/homwork/HomeWorkfour_.java
@@ -13,14 +13,13 @@ public class HomeWorkfour_ {
         int number = 0;
         String name = "";
         help();
-        boolean status=false;
+        boolean status = false;
         while (true) {
-            if(status==true) break;
+            if (status == true) break;
             try {
                 command = scanner.nextLine();
-
                 switch (command) {
-                    case "a": {
+                    case "a":
                         System.out.println("Введите ID человека");
                         number = scanner.nextInt();
                         scanner.nextLine();
@@ -28,37 +27,30 @@ public class HomeWorkfour_ {
                         name = scanner.nextLine();
                         System.out.println("Введите команду 's' для сохранения или 'w' для сохранения и просмотра:");
                         break;
-                    }
-                    case "w": {
+                    case "w":
                         map.put(number, name);
                         showPerson();
                         System.out.println("Введите команду:");
                         break;
-                        // System.out.println(map.entrySet());
-                    }
-                    case "r": {
+                    case "r":
                         showPerson();
                         System.out.println("Введите команду:");
                         break;
-                    }
-                    case "s": {
+                    case "s":
                         map.put(number, name);
                         System.out.println("Данные сохранены");
                         System.out.println("Введите команду:");
                         break;
-                    }
-                    case "h": {
+                    case "h":
                         help();
                         System.out.println("Введите команду:");
-                    }
-                    case "q": {
-                        status=true;
+                    case "q":
+                        status = true;
                         System.out.println("Досвидание!!!!!!!!!");
                         break;
-
-                    } default: {
+                    default:
                         System.out.println("Вы ввели что то другое");
-                    }
+
                 }
             } catch (Exception e) {
                 System.out.println("Введите число");
@@ -69,12 +61,7 @@ public class HomeWorkfour_ {
     }
 
     public static void help() {
-        System.out.println("Для того чтобы начать вводить данные введиет 'a'\n"
-                + "Для сохранения  изменения и просомтра введиет 'w'\n"
-                + "Для просомтра данных введиет 'r'\n"
-                + "Для сохранения  изменения введите 's'\n"
-                + "Для простмора команд введите 'h'\n"
-                + "Для выхода из системы без изменений введите 'q'");
+        System.out.println("Для того чтобы начать вводить данные введиет 'a'\n" + "Для сохранения  изменения и просомтра введиет 'w'\n" + "Для просомтра данных введиет 'r'\n" + "Для сохранения  изменения введите 's'\n" + "Для простмора команд введите 'h'\n" + "Для выхода из системы без изменений введите 'q'");
     }
 
     public static void showPerson() {
