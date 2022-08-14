f612f0563cc9bcfd905405c7f3b2f320563483e2
refat <refaazer@gmail.com>
2022-08-14 20:35:51 +0300

    alishev

--- src/main/java/homwork/HomWorkfive.java
+++ src/main/java/homwork/HomWorkfive.java
@@ -17,43 +17,46 @@ public class HomWorkfive {
         ///     !A | B                        =                  !(A & !B)
         ///     A | !B                        =                  !(!A & B)
         ///     A | B                         =                  !(!A & !B)
-       // boolean a = true;
-       // boolean b = false;
+        // boolean a = true;
+        // boolean b = false;
 
-        showOperation(true,false);
-        showOperation(true,true);
-        showOperation(false,true);
-        showOperation(false,false);
+        showOperation(true, false);
+        showOperation(true, true);
+        showOperation(false, true);
+        showOperation(false, false);
 
     }
-    private static void showOperation(boolean a, boolean b){
+
+    private static void showOperation(boolean a, boolean b) {
         // Условие до применения теоремы Де Моргана.
         System.out.println("Условие где A = " + a + " и где B = " + b);
-        System.out.println("    !A && !B > " + operation(!a && !b) + " == !(A || B) > " + operation(!(a || b)) + " "+info(!a && !b,!(a || b)));
+        System.out.println("    !A && !B > " + operation(!a && !b) + " == !(A || B) > " + operation(!(a || b)) + " " + info(!a && !b, !(a || b)));
 
-        System.out.println("    !A && B > " + operation(!a && b) + " == !(A || !B) > " + operation(!(a || !b))+ " "+info(!a && b ,!(a || !b)));
+        System.out.println("    !A && B > " + operation(!a && b) + " == !(A || !B) > " + operation(!(a || !b)) + " " + info(!a && b, !(a || !b)));
 
-        System.out.println("    A && !B > " + operation(a && !b) + " == !(!A || B) > " + operation(!(!a && b))+ " "+ info(a && !b,!(!a && b)));
+        System.out.println("    A && !B > " + operation(a && !b) + " == !(!A || B) > " + operation(!(!a && b)) + " " + info(a && !b, !(!a && b)));
 
-        System.out.println("    A && B > " + operation(a && b) + " == !(!A || !B) > " + operation(!(!a || !b))+ " "+ info(a && b,!(!a || !b)));
+        System.out.println("    A && B > " + operation(a && b) + " == !(!A || !B) > " + operation(!(!a || !b)) + " " + info(a && b, !(!a || !b)));
 
-        System.out.println("    !A || !B > " + operation(!a || !b) + " == !(A && B) > " + operation(!(a && b))+ " "+ info(!a || !b,!(a && b)));
+        System.out.println("    !A || !B > " + operation(!a || !b) + " == !(A && B) > " + operation(!(a && b)) + " " + info(!a || !b, !(a && b)));
 
-        System.out.println("    !A || B > " + operation(!a || b) + " ==  !(A && !B) > " + operation(!(a && !b))+ " "+ info(!a || b,!(a && !b)));
+        System.out.println("    !A || B > " + operation(!a || b) + " ==  !(A && !B) > " + operation(!(a && !b)) + " " + info(!a || b, !(a && !b)));
 
-        System.out.println("    A || !B > " + operation(a || !b) + " == !(!A & B) > " + operation(!(!a && b))+ " "+ info(a || !b,!(!a && b)));
+        System.out.println("    A || !B > " + operation(a || !b) + " == !(!A & B) > " + operation(!(!a && b)) + " " + info(a || !b, !(!a && b)));
 
-        System.out.println("    A || B = " + operation(a || b) + " ==  !(!A & !B) > " + operation(!(!a && !b))+ " "+ info(a || b,!(!a && !b)));
+        System.out.println("    A || B = " + operation(a || b) + " ==  !(!A & !B) > " + operation(!(!a && !b)) + " " + info(a || b, !(!a && !b)));
 
     }
+
     private static String operation(boolean a) {
         // Условие до применения теоремы Де Моргана.
         if (a) return "ИСТИННА";
         else return "ЛОЖ";
 
     }
-    private static String info(boolean a,boolean b){
-        if(a==b) return "(Де Морган РАБОТАЕТ)";
+
+    private static String info(boolean a, boolean b) {
+        if (a == b) return "(Де Морган РАБОТАЕТ)";
         else return "(Де Моргана НЕ РАБОТАЕТ Почему?)";
 
     }
