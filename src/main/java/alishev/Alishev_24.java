25a026fe1ba28f2b050f04f47cd71a0f914a67f1
refat <refaazer@gmail.com>
2022-08-15 20:59:46 +0300

    alishev

---
+++ src/main/java/alishev/Alishev_24.java
@@ -0,0 +1,12 @@
+package alishev;
+
+public class Alishev_24 {
+    public static void main(String[] args) {
+        String[] arrey = new String[]{"rifat", "How", "are", "you"};
+        StringBuilder myInfo = new StringBuilder("Start");
+        for (String arr : arrey) {
+            myInfo.append(" " + arr.toUpperCase());
+        }
+        System.out.println(myInfo);
+    }
+}
