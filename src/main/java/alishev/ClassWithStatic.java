2c5efae7a9fd7abdbcfe68652626b7afe4d8d993
refat <refaazer@gmail.com>
2022-08-15 21:12:14 +0300

    alishev

--- src/main/java/alishev/ClassWithStatic.java
+++ src/main/java/alishev/ClassWithStatic.java
@@ -2,13 +2,13 @@ package alishev;
 
 public class ClassWithStatic {
     public static void main(String[] args) {
-        String atr = "["+Mathematics.math("(5+3)*(6-4)*(3-1)")+"]";
-        System.out.printf("Это число PI равна %1s, а задача с примера (5+3)*(6-4)*(3-1) равна %2s",Mathematics.PI,atr);
+        String atr = "[" + Mathematics.math("(5+3)*(6-4)*(3-1)") + "]";
+        System.out.printf("Это число PI равна %1s, а задача с примера (5+3)*(6-4)*(3-1) равна %2s", Mathematics.PI, atr);
     }
 }
 
 class Mathematics {
-    public static final double PI=3.14159265359;
+    public static final double PI = 3.14159265359;
     private String atr;
 
     public Mathematics(String atr) {
@@ -32,7 +32,7 @@ class Mathematics {
 
             k = k * c;
         }
-        return  k;
+        return k;
     }
 
 }
