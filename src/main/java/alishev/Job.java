22a6a9b486aa0f1cb27310cbb5bbd096423e6578
refat <refaazer@gmail.com>
2022-08-15 22:04:56 +0300

    alishev

---
+++ src/main/java/alishev/Job.java
@@ -0,0 +1,49 @@
+package alishev;
+
+public class Job {
+   /**
+    * 1
+    Middle+Android-разработчика
+    Требования
+    Опыт работы с Java и Selenium
+    Опыт тестирования Java и веб-приложений, HTLM5
+
+    *2
+    Middle+Android-разработчика
+    Требования
+    Глубокие знания: Java, Kotlin;
+    БД: mySQL, postgreSQL;
+    Веб-серверы: nginx;
+
+    3*
+    ООО «Софтиндекс» ищет Java-разработчика
+    Требования
+    Уверенное знание Java
+    Отличное знание ООП, алгоритмов и структур данных;
+    Знание баз данных: SQL, NoSQL, навыки оптимизации запросов;
+
+    4*
+    Rocketech.IT ищет разработчика Middle+ IOS
+    Английский: Upper Intermediate
+    Требования
+    Глубокие знания: Java (8-11), Kotlin;
+    ️Опыт работы с: Spring Boot, JPA, Hibernate, Rest API, Web Sockets, RabbitMQ, Maven, JSP,
+
+    5*
+    Artezio ищет Java
+    Разработчик
+    ◾️Английский: Intermediate
+    Требования
+    ️Хорошее знание Java 8 и Spring.
+    Опыт использования Spring Boot + Spring Security + Spring Data (Hibernate) от 1 года.
+    ️Знание SQL (Flyway, Postgres).
+
+    Выделяю основные требования
+    1. Уверенное знание Java (8-11 и выще)
+    2. Отличное знание ООП, алгоритмов и структур данных;
+    3. Знание баз данных: SQL, NoSQL, навыки оптимизации запросов
+    4. Spring Boot + Spring Security + Spring Data
+    5. JPA, Hibernate, Rest API, Web Sockets, RabbitMQ, Maven, JSP
+
+    */
+}
