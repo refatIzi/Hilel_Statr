e45d77a31883377ff1f6697c9f33533cf97938dc
refat <refaazer@gmail.com>
2022-08-24 22:24:10 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/calculation/TestCalculation.java
+++ src/main/java/calculation/TestCalculation.java
@@ -6,13 +6,13 @@ public class TestCalculation {
         /**убедитесь что при вводе задачи вы установили закрывающие скобки
          *------------------------- как тут (((500+4)*2)-30) ---------------*/
         /***/
-        System.out.println(calculation.toSolve("((((400-2)-(100/2))/((4*4)/8))/((224-200)/12))"));//=87
+        System.out.println(calculation.check("((((400-2)-(100/2))/((4*4)/8))/((224-200)/12))"));//=87
 
-        //System.out.println(calculation.toSolve("((((400+2)-(2*3))/((6-2)/(3-1)))*((3-1+20)*3))"));//13068
-        //System.out.println(calculation.toSolve("500+4"));//=504
-        //System.out.println(calculation.toSolve("(((500+4)*2)-30)"));//=978
-        //System.out.println(calculation.toSolve("(1100-((500+4)*2))"));//=92
-        //System.out.println(calculation.toSolve("((30-2)*((500+4)*2))"));//=28224
+        //System.out.println(calculation.check("((((400+2)-(2*3))/((6-2)/(3-1)))*((3-1+20)*3))"));//13068
+        //System.out.println(calculation.check("500+4"));//=504
+        //System.out.println(calculation.check("(((500+4)*2)-30)"));//=978
+        //System.out.println(calculation.check("(1100-((500+4)*2))"));//=92
+        //System.out.println(calculation.check("((30-2)*((500+4)*2))"));//=28224
 
     }
 
