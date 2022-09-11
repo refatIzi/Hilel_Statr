e0c9dff0c93efc653aa210f09ebef2ca0f95f10f
refat <refaazer@gmail.com>
2022-09-11 23:40:06 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/homwork/homework10/Test.java
@@ -0,0 +1,13 @@
+package homwork.homework10;
+
+public class Test {
+    public static void main(String[] args) {
+        Animals animals=Animals.DOG;
+        switch (animals){
+            case CAT -> System.out.println("CAT Это "+animals);
+            case DOG -> System.out.println("DOG Это "+animals);
+            case FROG -> System.out.println("FROG Это "+animals);
+            default -> System.out.println("На животное "+animals+" это не похоже");
+        }
+    }
+}
