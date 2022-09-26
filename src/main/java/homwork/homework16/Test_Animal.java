df6130df3c648cbc4a13f1b9a49cc90d34892cfb
refat <refaazer@gmail.com>
2022-09-26 22:28:46 +0300

    Merge remote-tracking branch 'origin/master_Yoda' into master_Yoda

---
+++ src/main/java/homwork/homework16/Test_Animal.java
@@ -0,0 +1,14 @@
+package homwork.homework16;
+
+
+public class Test_Animal {
+    public static void main(String[] args) {
+        Animals cat = Animals.CAT;
+        Animals dog = Animals.DOG;
+        Animals frog = Animals.FROG;
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(frog);
+
+    }
+}
